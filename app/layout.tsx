c8fa3de161e1f67f42fbc086b6e6864fee43d238
Agnibha Ray <agnibharay.55@gmail.com>
2024-01-30 13:36:57 +0530

    Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from '@clerk/nextjs'
-
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +19,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
+      <SpeedInsights/>
         <body className={inter.className}>{children}</body>
       </html>
     </ClerkProvider>
